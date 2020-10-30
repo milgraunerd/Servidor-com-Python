5279e552334ec16b19d03b2288d63f9d6385ab82
milgraunerd <milgraunerd@gmail.com>
2020-10-30 19:00:03 +0000

    segundo commit

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World!!"
+
+@app.route('/tiago')
+def tiago():
+    return "Hello Tiago :-D"
+
+'''
+Ctrl + L - Limpar o terminal
+Subir para o github:
+1º - git add "nomedoarquivo.py"
+2º - git commit -m "descrição do commit aqui"
+3º - git push
+PRONTO!!
+'''
